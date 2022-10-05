b1dedce5daad30e8d5dbf527e2e945ad4767e177
ThomasCode92 <thomascode92@hotmail.com>
2022-10-05 12:37:12 +0200

    Adding Coaches to Vuex

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,14 @@
-export default {};
+export default {
+  registerCoach(context, data) {
+    const coachData = {
+      id: context.rootGetters.userId,
+      firstName: data.firstname,
+      lastName: data.lastname,
+      description: data.description,
+      areas: data.areas,
+      hourlyRate: data.rate,
+    };
+
+    context.commit('registerCoach', coachData);
+  },
+};
