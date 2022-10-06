da4d74dc253025584aaf73ce6186ed973c5053a3
ThomasCode92 <thomascode92@hotmail.com>
2022-10-06 13:02:48 +0200

    Filtering Requests for the Active Coach

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,13 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, getters, rootState, rootGetters) {
+    const coachId = rootGetters.userId;
+    const requests = state.requests.filter(
+      request => request.coachId === coachId
+    );
+
+    return requests;
   },
-  hasRequests(state) {
-    return state.requests && state.requests.length > 0;
+  hasRequests(state, getters) {
+    return getters.requests && getters.requests.length > 0;
   },
 };
