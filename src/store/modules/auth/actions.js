f78593d01e537d685be8192f544328d04a9e7ae6
ThomasCode92 <thomascode92@hotmail.com>
2022-10-12 12:57:14 +0200

    Adding a Login Action & Flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -4,6 +4,7 @@ const FIREBASE_API_KEY = 'AIzaSyAUajXj2o2O2m54aE_qLF6kzzzduzxEqfw';
 export default {
   async signup(context, payload) {
     const FIREBASE_URL = FIREBASE_BASE_URL + `:signUp?key=${FIREBASE_API_KEY}`;
+
     const response = await fetch(FIREBASE_URL, {
       method: 'POST',
       body: JSON.stringify({
@@ -19,7 +20,35 @@ export default {
       const errorMessage = responseData.message || 'Failed to authenticate!';
       const error = new Error(errorMessage);
 
-      console.log(responseData);
+      throw error;
+    }
+
+    console.log(responseData);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+  async login(context, payload) {
+    const FIREBASE_URL =
+      FIREBASE_BASE_URL + `:signInWithPassword?key=${FIREBASE_API_KEY}`;
+
+    const response = await fetch(FIREBASE_URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = responseData.message || 'Failed to authenticate!';
+      const error = new Error(errorMessage);
 
       throw error;
     }
