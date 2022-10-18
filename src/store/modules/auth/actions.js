6f3c005d1c0291f6bad776fe6fdda20d7ccb5e18
ThomasCode92 <thomascode92@hotmail.com>
2022-10-18 12:40:28 +0200

    Adding "Auto Logout"

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,8 @@
 const FIREBASE_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts';
 const FIREBASE_API_KEY = 'AIzaSyAUajXj2o2O2m54aE_qLF6kzzzduzxEqfw';
 
+let timer;
+
 export default {
   async signup(context, payload) {
     return context.dispatch('auth', {
@@ -42,32 +44,56 @@ export default {
       throw error;
     }
 
+    const expiresIn = parseInt(responseData.expiresIn) * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+
     localStorage.setItem('token', responseData.idToken);
     localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(function () {
+      context.dispatch('autoLogout');
+    }, expiresIn);
 
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
     });
   },
   autoLogin(context) {
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const expirationDate = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = parseInt(expirationDate) - new Date().getTime();
+
+    if (expiresIn < 10000) return;
+
+    timer = setTimeout(function () {
+      context.dispatch('autoLogout');
+    }, expiresIn);
 
     if (token && userId) {
       context.commit('setUser', {
         token: token,
         userId: userId,
-        tokenExpiration: null,
       });
     }
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+    clearTimeout(timer);
+
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   },
 };
